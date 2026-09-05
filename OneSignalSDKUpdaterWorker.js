b8b3bbb13be9cd556c27aa2d412a5e6eeca0086e
Occhionero <164205096+Naiontrack@users.noreply.github.com>
2026-09-05 21:05:24 +0200

    Add files via upload

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,1 +1,9 @@
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
